92daf93724669a4f92b9846024922d8b08ff6f64
mpbill <mpbill@gmail.com>
2016-12-28 23:55:53 -0600

    started working on token generation

---
+++ Ca.Scta.Account/AppUserClaimsIdentityFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Ca.Scta.Account
+{
+    public class AppUserClaimsIdentityFactory : IAppUserClaimsIdentityFactory
+    {
+        public ClaimsIdentity CreateClaimsIdentity(AppUser user)
+        {
+            var idClaim = new Claim("UserId",user.Id.ToString(),ClaimValueTypes.Integer32);
+            var userNameClaim = new Claim("UserName", user.UserName);
+            var identity = new AppUserIdentity(user.UserName);
+            var claims = new List<Claim> {idClaim, userNameClaim};
+            var ci = new ClaimsIdentity(identity, claims);
+            return ci;
+
+        }
+    }
+}
---
+++ Ca.Scta.Account/IAppUserClaimsIdentityFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Ca.Scta.Account
+{
+    public interface IAppUserClaimsIdentityFactory
+    {
+        ClaimsIdentity CreateClaimsIdentity(AppUser user);
+    }
+}
