323531b304b5f9abb7f7e34fa6ce3e2fd73972ed
mpbill <mpbill@gmail.com>
2016-12-28 22:28:46 -0600

    finished add app user command

--- Ca.Scta.Dal/Cqrs/AddAppUser.cs
+++ Ca.Scta.Dal/Cqrs/AddAppUser.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ca.Scta.Dal.Connection;
 using Ca.Scta.Dal.Cqrs.Base;
+using Ca.Scta.Dal.Models;
+using Dapper;
 
 namespace Ca.Scta.Dal.Cqrs
 {
@@ -24,5 +27,43 @@ namespace Ca.Scta.Dal.Cqrs
         public string Email { get; private set; }
         public bool EmailConfirmed { get; private set; }
     }
-    public class 
+
+    public class AddAppUserCommandHandler : DapperCommandHandler<AddAppUser, int>
+    {
+        private readonly string _sql;
+        public AddAppUserCommandHandler(IDbConnectionAsyncFactory factory) : base(factory)
+        {
+            _sql = @"
+                    INSERT INTO AppUsers
+                    (
+	                    UserName,
+	                    Email,
+	                    EmailConfirmed,
+	                    PasswordHash,
+	                    SecurityStamp
+                    )
+                    VALUES
+                    (
+	                    @UserName,
+	                    @Email,
+	                    @EmailConfirmed,
+	                    @PasswordHash,
+	                    @SecurityStamp
+                    );
+                    SELECT SCOPE_IDENTITY();
+                    ";
+        }
+
+        public override async Task<int> HandleAsync(AddAppUser command)
+        {
+            int scopeIdentity;
+            using (var conn = await Factory.GetOpenConnectionAsync())
+            {
+                scopeIdentity = await conn.ExecuteScalarAsync<int>(_sql, command);
+                conn.Close();
+            }
+            return scopeIdentity;
+
+        }
+    }
 }
