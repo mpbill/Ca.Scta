a46831b4f8aec163e3f5f035d25ea8dd4ae0b4b5
mpbill <mpbill@gmail.com>
2016-12-29 18:19:25 -0600

    created account test project.

---
+++ Ca.Scta.Account.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ca.Scta.Account.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
