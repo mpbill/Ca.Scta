915c5c3469e261e0b9d12e95a416c92f5963d1d3
mpbill <mpbill@gmail.com>
2016-12-31 20:49:14 -0600

    implamented delete and update contact command handlers in contact api endpoint.

--- Ca.Scta.Api/Controllers/Contacts/ContactsController.cs
+++ Ca.Scta.Api/Controllers/Contacts/ContactsController.cs
@@ -15,16 +15,22 @@ namespace Ca.Scta.Api.Controllers.Contacts
         private readonly DapperCommandHandler<CreateContactCommand, GenericResult<int>> _createContactCommandHandler;
         private readonly DapperQueryHandler<GetAllContactsQuery, List<Contact>> _getAllContactsQueryHandler;
         private readonly DapperQueryHandler<GetContactByIdQuery, Contact> _getContactByIdQueryHandler;
+        private readonly DapperCommandHandler<DeleteContactByIdCommand, GenericResult<int>> _deleteContactByIdCommandHandler;
+        private readonly DapperCommandHandler<UpdateContactByIdCommand, GenericResult<int>> _updateContactByIdCommandHandler;
 
         public ContactsController(
             DapperCommandHandler<CreateContactCommand,GenericResult<int>> createContactCommandHandler,
             DapperQueryHandler<GetAllContactsQuery,List<Contact>> getAllContactsQueryHandler,
-            DapperQueryHandler<GetContactByIdQuery,Contact> getContactByIdQueryHandler  
+            DapperQueryHandler<GetContactByIdQuery,Contact> getContactByIdQueryHandler,
+            DapperCommandHandler<DeleteContactByIdCommand,GenericResult<int>> deleteContactByIdCommandHandler,
+            DapperCommandHandler<UpdateContactByIdCommand,GenericResult<int>> updateContactByIdCommandHandler    
             )
         {
             _createContactCommandHandler = createContactCommandHandler;
             _getAllContactsQueryHandler = getAllContactsQueryHandler;
             _getContactByIdQueryHandler = getContactByIdQueryHandler;
+            _deleteContactByIdCommandHandler = deleteContactByIdCommandHandler;
+            _updateContactByIdCommandHandler = updateContactByIdCommandHandler;
         }
 
         [HttpPost]
@@ -68,6 +74,76 @@ namespace Ca.Scta.Api.Controllers.Contacts
             var responseModel = new ContactResponseViewModel(contact);
             return Ok(responseModel);
         }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IHttpActionResult> DeleteContactById(int id)
+        {
+            var command = new DeleteContactByIdCommand(id);
+            var result = await _deleteContactByIdCommandHandler.HandleAsync(command);
+            if (result.Succeeded)
+            {
+                var responseModel = new DeletedContactResultModel(id);
+                return Ok(responseModel);
+            }
+            switch (result.ErrorReason)
+            {
+                case (ErrorReason.NotFound):
+                    return NotFound();
+                default:
+                    return InternalServerError();
+            }
+        }
+
+        [HttpPut]
+        [Route]
+        public async Task<IHttpActionResult> UpdateContactById(UpdateContactRequestModel model)
+        {
+            var cmd = new UpdateContactByIdCommand(
+                model.City,
+                model.Name,
+                model.Description,
+                model.Position,
+                model.Email,
+                model.ContactId);
+            var result = await _updateContactByIdCommandHandler.HandleAsync(cmd);
+            if (result.Succeeded)
+            {
+                var responseModel = new UpdateContactResponseModel(result.Data);
+                return Ok(responseModel);
+            }
+            switch (result.ErrorReason)
+            {
+                case ErrorReason.NotFound:
+                    return NotFound();
+
+                default:
+                    return InternalServerError();
+            }
+        }
         
+    }
+
+    public class UpdateContactResponseModel
+    {
+        public UpdateContactResponseModel(int id)
+        {
+            UpdatedContactId = id;
+        }
+
+        public int UpdatedContactId { get; set; }
+    }
+
+    public class UpdateContactRequestModel : CreateContactViewModel
+    {
+        public int ContactId { get; set; }
+    }
+    public class DeletedContactResultModel
+    {
+        public DeletedContactResultModel(int id)
+        {
+            this.DeletedContactId = id;
+        }
+
+        public int DeletedContactId { get; set; }
     }
 }
